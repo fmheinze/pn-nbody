8bc28db0f92c5d1b24ffad03f6d1f23b57f5bc7c
Felix Heinze <f-heinze@outlook.com>
2025-02-07 01:04:55 +0100

    Add maximum time step to adaptive time stepping, use file output functions

---
+++ src/output.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "pn_eom.h"
+
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+void general_output_init(FILE** file, struct ode_params* params);
+void general_output_write(FILE* file, struct ode_params* params, double* w, double t);
+void eccentricity_output_init(FILE** file, struct ode_params* params);
+void eccentricity_output_write(FILE* file, struct ode_params* params, double* w, double t);
+void semi_major_axes_output_init(FILE** file, struct ode_params* params);
+void semi_major_axes_output_write(FILE* file, struct ode_params* params, double* w, double t);
+
+#endif
